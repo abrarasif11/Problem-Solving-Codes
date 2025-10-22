160393d529965e282bd99e6e1904832d896fa6d4
abrarasif11 <faasif929@gmail.com>
2025-10-22 22:26:19 +0600

    gcd

---
+++ Codeforces/GCD.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+int a,b;
+int gcd;
+cin >> a >> b;
+for(int i = 1; i <= a && i <= b; i++){
+    if(a%i == 0 && b%i == 0){
+        gcd = i;
+    }
+}
+    cout<<gcd<<endl;
+return 0;
+}
