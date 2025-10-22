2d9273052a568aefb97dec2c081acbf136162d91
abrarasif11 <faasif929@gmail.com>
2025-10-22 22:39:07 +0600

    fix pass

---
+++ Codeforces/Sheet #2 (Loops)/Fixed Password.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+int a;
+while(true){
+    cin >> a;
+    if(a == 1999){
+        cout<<"Correct"<<endl;
+        break;
+    }
+    else{
+        cout<<"Wrong"<<endl;
+    }
+}
+return 0;
+}
