0460d13c37b1d670b2761b3d3e557520c79c5f48
abrarasif11 <faasif929@gmail.com>
2025-10-31 20:56:29 +0600

    consump

---
+++ BeeCrowd/Consumption.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+double x,y,total;
+cin >> x;
+cin >> y;
+total =x/y;
+cout <<fixed<<setprecision(3)<<total <<" km/l"<<endl;
+return 0;
+}
