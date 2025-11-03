e8423c16c284451e9a6b3a9fd87c3a0750206299
abrarasif11 <faasif929@gmail.com>
2025-11-03 19:25:27 +0600

    diff

---
+++ BeeCrowd/Difference.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+int A;
+int B;
+int C;
+int D;
+int def;
+cin >> A;
+cin >> B;
+cin >> C;
+cin >> D;
+def = (A * B - C * D);
+cout <<"DIFERENCA = "<<def<< endl;
+return 0;
+}
