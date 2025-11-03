de59c0e82c419f3cfaf0c96fba8125d5ab4d1acb
abrarasif11 <faasif929@gmail.com>
2025-11-03 19:19:31 +0600

    salary

---
+++ BeeCrowd/Salary.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+int number;
+cin >> number;
+double x,y,salary;
+cin >> x;
+cin >> y;
+salary = x*y;
+cout <<"NUMBER"<< " = "<< number<< endl;
+cout <<fixed<<setprecision(2)<<"SALARY"<< " = " <<"U$ "<< salary<< endl;
+return 0;
+}
