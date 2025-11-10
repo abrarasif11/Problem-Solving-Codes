3a3bfd46e6a9724d156f270abf25a0f1f2487061
abrarasif11 <faasif929@gmail.com>
2025-11-10 22:33:45 +0600

    good kid

---
+++ Codeforces/Good Kid.cpp
@@ -0,0 +1,26 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+int a;
+cin >> a;
+while(a--){
+int n;
+cin >> n;
+int x[n];
+int mini = 0;
+for(int i = 0; i < n; i++){
+cin>>x[i];
+if(x[i] < x[mini]){
+    mini = i;
+            }
+        }
+x[mini] += 1;
+int p = 1;
+for(int i = 0; i < n; i++){
+    p *= x[i];
+        }
+    cout<<p <<endl;
+    }
+return 0;
+}
